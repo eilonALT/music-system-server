4746940ee19515f8a0df4e4829acb590f4029ee3
eilon <eilon159123@gmail.com>
2022-06-11 13:59:05 +0300

    node init

---
+++ server/server.js
@@ -0,0 +1,17 @@
+import express from 'express';
+import cors from 'cors';
+import { accountsRouter } from './controllers/accounts-controller.js';
+import { collectionsRouter } from './controllers/collections-controller.js';
+import { songsRouter } from './controllers/songs-controller.js';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use("/api/music-system/", accountsRouter);
+app.use("/api/music-system/", collectionsRouter);
+app.use("/api/music-system/", songsRouter);
+
+
+app.listen(5000,()=>{
+    console.log('Server is running on port 5000');
+})
