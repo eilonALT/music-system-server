a497b08585ddb6ee59068e0b5a9704a5b5ba7c30
eilon <eilon159123@gmail.com>
2022-06-11 23:21:19 +0300

    dokcer check

--- server/data-access-layer/db.js
+++ server/data-access-layer/db.js
@@ -2,12 +2,6 @@ import mysql from 'mysql2';
 import dotenv from 'dotenv';
 dotenv.config();
 
-// const PASSWORD = process.env.PASSWORD;
-// const USER_NAME = process.env.USER_NAME;
-// const HOST = process.env.HOST;
-// const DATABASE = process.env.DATABASE;
-// const PORT = process.env.PORT;
-
 const connection = mysql.createConnection({
     host: 'mysqldb',
     user: 'root',
